b9b96918685be832acf7f8fa00941ebfc925416f
gdunix <giorgosdounis@gmail.com>
2024-05-27 00:33:13 +0300

    Fixed vendors on mobile

--- app/vendors/[[...slug]]/title/Title.tsx
+++ app/vendors/[[...slug]]/title/Title.tsx
@@ -10,13 +10,13 @@ const Title: React.FC<Props> = ({ category = "", city = '', results = 0 }: Props
   const categoryName = U.getCategoryBySlug(category);
   const cityName = city ? U.getCityBySlug(city) : '';
   return (
-    <div className="text-6xl mt-4 flex">
-      <span>{results} results for</span>{" "}
-      <span className="text-primary mx-2">{categoryName}</span>
+    <div className="text-6xl mt-4 flex flex-col md:flex-row">
+      <div className="mr-1">{results} results for</div>
+      <div className="text-primary mx-0 md:mx-2">{categoryName}</div>
       {!!city && (
-        <>
-          <span>near</span> <span className="text-primary mx-2">{cityName}</span>
-        </>
+        <div className="flex">
+          <div>near</div> <div className="text-primary mx-2">{cityName}</div>
+        </div>
       )}
     </div>
   );
